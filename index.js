b976dd404ab35f594fa35c6468027ec353dd91b8
Sarah Desmarina <30022131sarah@gmail.com>
2026-02-24 15:18:45 +0700

    Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+// app.use((req, res, next) => {
+//   console.log(`Request ${req.path} harus lewat sini`);
+//   next();
+// });
+// const auth = (req, res, next) => {
+//   console.log('Auth middleware dijalankan');
+//   next();
+// };
+// app.use(auth);
+// // app.get('/', (req, res) => {
+// //   res.send('Halo, nama saya Sarah');
+// // });
+app.use((req, res, next) => {
+  if (false) {
+    next(new Error('Not Found'));
+    return;  
+  }
+  next();
+});
+app.get('/say/:greeting', (req, res) => {
+  const { greeting } = req.params;
+  res.send(greeting);
+});
+
+app.get('/',(req, res, next) => {
+  res.send('Hello Express');
+})
+
+app.use((err, req, res, next) => {
+  res.send("Error Occurred");
+})
+
+app.listen(3000)
